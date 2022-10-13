ec9911682823c24fda3baece34a7438b80d26d4c
Vlad <va.panin@edu.hse.ru>
2022-10-13 12:26:33 +0300

    first

---
+++ 1.py
@@ -0,0 +1,11 @@
+array = []
+for n in range(4):
+    n = int(input("Введите массив array:"))
+    array.append(n)
+delta = int(input("Введите значение Delta:"))
+min_array = min(array)
+c = 0
+for i in array:
+    if (i - delta) == min_array:
+        c += 1
+print(c)
